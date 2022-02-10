14a4d7500d024da1a17e6bbf5698abca68175193
WatsonJay <446725026@qq.com>
2022-02-10 09:59:29 +0800

    update

--- air_collect_arduino/data/js/main.js
+++ air_collect_arduino/data/js/main.js
@@ -1,3 +1,32 @@
+var wifiInfoInterval;
+var mtqqInfoInterval;
+
+$(document).ready(function (){
+    $('#wifiCollapse').on('show.bs.collapse', function () {
+        refreshWifiList()
+    })
+});
+
+function changeWifiCollapse(){
+    $("#wifiCollapse").collapse("toggle");
+}
+
+function refreshWifiInfo(){
+    wifiInfoInterval = setInterval(function () {
+        console.log("执行")
+    },2000)
+}
+
+function refreshWifiList(){
+    $.getJSON("/wifiInfo",function (data) {
+        
+    })
+}
+
+window.onbeforeunload = function () {
+    window.clearInterval(wifiInfoInterval)
+}
+
 function clearDom() {
     $("#mtqqServer").empty();
 }
