782d50418730bd244d9fd78775271722dd98adb4
WatsonJay <446725026@qq.com>
2022-02-26 22:32:43 +0800

    update

--- air_collect_arduino/data/js/main.js
+++ air_collect_arduino/data/js/main.js
@@ -2,8 +2,8 @@ var wifiInfoInterval;
 var mtqqInfoInterval;
 
 $(document).ready(function (){
+    refreshWifiInfo()
     $('#wifiCollapse').on('show.bs.collapse', function () {
-        refreshWifiInfo()
         refreshWifiList()
     })
 });
@@ -13,14 +13,23 @@ function changeWifiCollapse(){
 }
 
 function refreshWifiInfo(){
-    wifiInfoInterval = setInterval(function () {
-        console.log("执行")
-    },2000)
+    $.getJSON("/wifiInfo",function (data) {
+
+    })
+    // wifiInfoInterval = setInterval(function () {
+    //     console.log("执行")
+    // },4000)
 }
 
 function refreshWifiList(){
     $.getJSON("/wifiList",function (data) {
-        
+        debugger
+        var list = data.result.sort(function(a, b){
+            return parseInt(a["RSSI"]) < parseInt(b["RSSI"]) ? 1 : parseInt(a["RSSI"]) == parseInt(b["RSSI"]) ? 0 : -1;
+        });
+        $.each(list,function(index,obj){
+            $("#wifiInfo").append("<option value='"+obj["SSID"]+"'>"+obj["SSID"]+"</option>");
+        })
     })
 }
 
